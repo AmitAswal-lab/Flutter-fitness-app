21423cd257d4ca8277bfab86fef79eee69dae5e5
Amit Aswal <gametheory284@gmail.com>
2025-12-17 08:16:20 +0530

    chore: setup dependency injection with GetIt for steps feature

---
+++ lib/injection_container.dart
@@ -0,0 +1,33 @@
+import 'package:fitness_app/features/steps/data/datasources/pedometer_datasource.dart';
+import 'package:fitness_app/features/steps/data/datasources/step_local_datasource.dart';
+import 'package:fitness_app/features/steps/data/repositories/step_repository_impl.dart';
+import 'package:fitness_app/features/steps/domain/repositories/step_repository.dart';
+import 'package:fitness_app/features/steps/domain/usecases/get_daily_steps.dart';
+import 'package:fitness_app/features/steps/domain/usecases/get_step_stream.dart';
+import 'package:fitness_app/features/steps/domain/usecases/get_weekly_steps.dart';
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  //UseCases
+  sl.registerLazySingleton(() => GetStepStream(sl()));
+  sl.registerLazySingleton(() => GetDailySteps(sl()));
+  sl.registerLazySingleton(() => GetWeeklySteps(sl()));
+
+  //Repositories
+  sl.registerLazySingleton<StepsRepository>(
+    () => StepRepositoryImpl(
+      pedometerDatasource: sl(),
+      stepLocalDatasource: sl(),
+    ),
+  );
+
+  //DataSources
+  sl.registerLazySingleton<StepLocalDatasource>(
+    () => StepLocalDatasourceImpl(sharedPreferences: sl()),
+  );
+  sl.registerLazySingleton<PedometerDataSource>(
+    () => PedometerDataSourceImpl(),
+  );
+}
